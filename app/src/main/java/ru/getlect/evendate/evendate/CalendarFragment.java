f4b39d3311c96ad07c35f35c6af37643cd28338a
Gordeev Dmitry <dimgeneral@mail.ru>
2015-10-09 04:24:03 +0400

    added view pager and pagertabstrip in main activity added adapter for view pager added feed string into resources renamed activity_calendar to fragment_calendar removed toolbar from calendar fragment changed main activity layout fixed typo in constant name

--- app/src/main/java/ru/getlect/evendate/evendate/CalendarFragment.java
+++ app/src/main/java/ru/getlect/evendate/evendate/CalendarFragment.java
@@ -18,7 +18,7 @@ public class CalendarFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_calendar, container, false);
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
 
 
 //
