a9d712ac2aeb17843ff13aec0e90f3424150a9ab
Дмитрий Гордеев <dimgeneral@mail.ru>
2016-02-22 09:07:10 +0400

    updated user profile layout added custom behavior for avatar cover

--- app/src/main/java/ru/evendate/android/ui/UserProfileActivity.java
+++ app/src/main/java/ru/evendate/android/ui/UserProfileActivity.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -47,7 +46,7 @@ public class UserProfileActivity extends AppCompatActivity implements LoaderList
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white);
         //make status bar transparent
-        ((AppBarLayout)findViewById(R.id.app_bar_layout)).addOnOffsetChangedListener(new StatusBarColorChanger(this));
+        //((AppBarLayout)findViewById(R.id.app_bar_layout)).addOnOffsetChangedListener(new StatusBarColorChanger(this));
 
         Intent intent = getIntent();
         if(intent != null){
